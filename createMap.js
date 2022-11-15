cd990fa3906bd421f4a1b4cdb981ea69bf5adc9c
hanktwichell <78049692+hanktwichell@users.noreply.github.com>
2022-11-15 10:15:13 -0800

    Update and rename w3d5.js to createMap.js

--- createMap.js
+++ createMap.js
@@ -1,3 +1,10 @@
+function createMap(keys, vals) {
+    var result = {};
+    for (var i=0; i<keys.length; i++)
+        result[keys[i]] = vals[i];
+    return result;
+}
+
 const keys1 = ["abc", 3, "yo"];
 const vals1 = [42, "wassup", true];
 const expected1 = {
@@ -11,10 +18,4 @@ const vals2 = [];
 const expected2 = {
 }
 
-function createMap(keys, vals) {
-    var result = {};
-    for (var i=0; i<keys.length; i++)
-        result[keys[i]] = vals[i];
-    return result;
-}
 console.log(createMap(keys1, vals1));
